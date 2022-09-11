a217ee28cf6676c52841859fa3b0b7c9cb28c34e
Alexander Verbruggen <alex@nabu.be>
2022-09-11 13:16:54 +0200

    updated

--- environment/public/artifacts/services/Environment.js
+++ environment/public/artifacts/services/Environment.js
@@ -5,15 +5,15 @@ Vue.service("environment", {
 	services: ["swagger"],
 	data: function() {
 		return {
-			settings: {}
+			settings: {
+				data: {}
+			}
 		}
 	},
 	activate: function(done) {
 		var self = this;
 		this.$services.swagger.execute("nabu.web.core.rest.environment").then(function(result) {
 			if (result) {
-				nabu.utils.objects.merge(self.settings, result);
-				
 				var scheme = result.scheme ? result.scheme : window.location.protocol.replace(":", "");
 				var host = result.host ? result.host : window.location.host;
 				// hard to make an educated guess from the url...
@@ -28,11 +28,29 @@ Vue.service("environment", {
 				if (result.language) {
 					application.configuration.applicationLanguage = result.language;
 				}
+
+				// merge it reactively into settings		
+				Object.keys(result).forEach(function(x) {
+					if (x != "data") {
+						Vue.set(self.settings, x, result[x])
+					}
+				});
+				// data as well
+				if (result.data) {
+					result.data.forEach(function(x) {
+						Vue.set(self.settings.data, x.type, x.content);
+					});
+				}
 			}
 			done();
 		}, function(error) {
 			console.log("Could not load environment information", error);
 			done();
 		});
+	},
+	methods: {
+		get: function(type) {
+			return this.settings.data[type];
+		}
 	}
 });
